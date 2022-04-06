fac3f0da7ee67010fa0c6ef4b1afcbe8ee832863
Thiago Rodrigues <thiagothogs@icloud.com>
2022-04-06 16:53:20 -0300

    update code structure

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,6 @@
-import 'firebase/auth';
-import 'firebase/database';
-import firebase from "firebase/compat";
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
 
 
 const firebaseConfig = {
@@ -15,5 +15,32 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
-const database = firebase.database();
+const database = firebase.firestore();
 export {firebase, auth, database}
+
+
+
+
+
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
+//
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBOK7x5N5UnjY4TDqndzH7l5tvdNIsWFRc",
+//   authDomain: "todo-app-e3cf0.firebaseapp.com",
+//   projectId: "todo-app-e3cf0",
+//   storageBucket: "todo-app-e3cf0.appspot.com",
+//   messagingSenderId: "940016886081",
+//   appId: "1:940016886081:web:91686613f16b1b1f8001c0",
+//   measurementId: "G-JHPC7TP12K"
+// };
+//
+// // Use this to initialize the firebase App
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+//
+// // Use these for db & auth
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+//
+// export { auth, db };
